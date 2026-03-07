924327d9d923b678231fdc31ab1dea3027d61c9f
Denis <d.bahdanousk@student.uw.edu.pl>
2026-03-07 11:18:38 +0100

    Experience texts and Vite routing fixes

--- vite.config.js
+++ vite.config.js
@@ -6,16 +6,21 @@ import { readdirSync } from "fs";
 // Collect all HTML files in the ceilor directory for multi-page build
 function getHtmlEntries(dir) {
   const entries = {};
-  const files = readdirSync(dir, { withFileTypes: true, recursive: true });
-  for (const file of files) {
-    if (file.isFile() && file.name.endsWith('.html')) {
-      const fullPath = resolve(file.parentPath || file.path, file.name);
-      const relativePath = fullPath
-        .replace(resolve(dir) + '/', '')
-        .replace('.html', '');
-      entries[relativePath] = fullPath;
+  function traverse(currentDir) {
+    const files = readdirSync(currentDir, { withFileTypes: true });
+    for (const file of files) {
+      if (file.isDirectory()) {
+        traverse(resolve(currentDir, file.name));
+      } else if (file.isFile() && file.name.endsWith('.html')) {
+        const fullPath = resolve(currentDir, file.name);
+        const relativePath = fullPath
+          .replace(resolve(dir) + '/', '')
+          .replace('.html', '');
+        entries[relativePath] = fullPath;
+      }
     }
   }
+  traverse(resolve(dir));
   return entries;
 }
 
